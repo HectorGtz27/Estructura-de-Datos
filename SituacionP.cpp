e040764127660be5638cd53cfb49c75bf8442225
Hector Alan Gutierrez Galvez <114173304+HectorGtz27@users.noreply.github.com>
2023-09-06 13:57:35 -0700

    Update SituacionP.cpp

--- SituacionP.cpp
+++ SituacionP.cpp
@@ -1,4 +1,4 @@
-//SituacionP.cpp
+// SituacionP.cpp
 #include "SituacionP.h"
 #include <vector>
 #include <iostream>
@@ -8,47 +8,74 @@ using namespace std;
 Registro::Registro(const std::string& f, const std::string& h, char p, const std::string& u)
     : fecha(f), hora(h), punto_entrada(p), ubi(u) {}
 
-//Merge para MergeSort
-void Registro::Merge(std::vector<int>& nums, int inicio, int mitad, int fin) {
-    vector<int> aux(fin + 1); // Vector auxiliar para almacenar el resultado fusionado
-    int i = inicio, j = mitad + 1, k = inicio;
+void Registro::ordenaMerge(std::vector<Registro>& registros, int inicio, int fin) {
+    if (inicio < fin) {
+        int mitad = (inicio + fin) / 2;
+        ordenaMerge(registros, inicio, mitad); // "Conquer"
+        ordenaMerge(registros, mitad + 1, fin); // "Conquer"
+        merge(registros, inicio, mitad, fin); // "Combine"
+    }
+}
 
-    while (i <= mitad && j <= fin) {
-        if (nums[i] < nums[j]) { // Comparar los elementos de los subarreglos
-            aux[k] = nums[i];
+void Registro::merge(std::vector<Registro>& registros, int inicio, int mitad, int fin) {
+    int n1 = mitad - inicio + 1;
+    int n2 = fin - mitad;
+
+    vector<Registro> L(n1);
+    vector<Registro> R(n2);
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = registros[inicio + i];
+    }
+    for (int i = 0; i < n2; i++) {
+        R[i] = registros[mitad + 1 + i];
+    }
+
+    int i = 0, j = 0, k = inicio;
+    while (i < n1 && j < n2) {
+        // Comparación de UBI primero y luego fecha
+        if (L[i].ubi + L[i].fecha < R[j].ubi + R[j].fecha) {
+            registros[k] = L[i];
             i++;
         }
         else {
-            aux[k] = nums[j];
+            registros[k] = R[j];
             j++;
         }
         k++;
     }
 
-    while (i <= mitad) {
-        aux[k] = nums[i];
+    while (i < n1) {
+        registros[k] = L[i];
         i++;
         k++;
     }
 
-    while (j <= fin) {
-        aux[k] = nums[j];
+    while (j < n2) {
+        registros[k] = R[j];
         j++;
         k++;
-    }
-
-    for (k = inicio; k <= fin; k++) {
-        nums[k] = aux[k];
     }
 }
 
 
-//Ordenamiento por MergeSort
-void Registro::oredenaMerge(std::vector<int>& nums, int inicio, int fin) {
-    if (inicio < fin) {
-        int mitad = (inicio + fin) / 2;
-        oredenaMerge(nums, inicio, mitad); // "Conquer"
-        oredenaMerge(nums, mitad + 1, fin); // "Conquer"
-        Merge(nums, inicio, mitad, fin); // "Combine"
+int Registro::busquedaBinaria(const std::vector<Registro>& registros, const std::string& serieABuscar) {
+    int bajo = 0;
+    int alto = registros.size() - 1;
+
+    while (bajo <= alto) {
+        int central = (bajo + alto) / 2;
+        const std::string& ubiCentral = registros[central].ubi;
+        if (ubiCentral.substr(0, 3) == serieABuscar) {
+            return central;
+        }
+        else if (serieABuscar < ubiCentral.substr(0, 3)) {
+            alto = central - 1;
+        }
+        else {
+            bajo = central + 1;
+        }
     }
+
+    return -1;
 }
