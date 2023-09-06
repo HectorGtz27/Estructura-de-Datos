f6c4aba6598490fef103000b6950921032907de1
Hector Alan Gutierrez Galvez <114173304+HectorGtz27@users.noreply.github.com>
2023-09-06 13:07:01 -0700

    Update SituacionP.h

--- SituacionP.h
+++ SituacionP.h
@@ -1,1 +1,21 @@
+//SituacionP.h
 #pragma once
+#include <vector>
+#include <string>
+
+class Registro {
+public:
+	std::string fecha;
+	std::string hora;
+	char punto_entrada;
+	std::string ubi;
+
+	Registro(const std::string& f, const std::string& h, char p, const std::string& u);
+	void Merge(std::vector<int>& nums, int inicio, int mitad, int fin);
+	void oredenaMerge(std::vector<int>& nums, int inicio, int fin);
+
+private:
+
+
+
+};
