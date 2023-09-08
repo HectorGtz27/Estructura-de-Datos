c6e8d2dca57f77befc7e5646b082583a1df4201b
Hector Alan Gutierrez Galvez <114173304+HectorGtz27@users.noreply.github.com>
2023-09-08 01:24:50 -0700

    Update SituacionP.h

--- SituacionP.h
+++ SituacionP.h
@@ -14,7 +14,10 @@ public:
     Registro() : fecha(""), hora(""), punto_entrada('\0'), ubi("") {}
 
     Registro(const std::string& f, const std::string& h, char p, const std::string& u);
+    //staic es para proporcionar una definición de la función que no está vinculada a un objeto en particular
+    // y se puede llamar sin crear un objeto.
     static void ordenaMerge(std::vector<Registro>& registros, int inicio, int fin);
     static void merge(std::vector<Registro>& registros, int inicio, int mitad, int fin);
     static int busquedaBinaria(const std::vector<Registro>& registros, const std::string& serieABuscar);
+
 };
